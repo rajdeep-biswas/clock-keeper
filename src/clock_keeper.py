b09c08c2a6f6bbdb00536719e071f7da523ec02d
Biswas <rajdeep.biswas@sap.com>
2022-01-19 16:03:53 +0530

    added code and folder structure

---
+++ src/clock_keeper.py
@@ -0,0 +1,31 @@
+"""
+Author: r4jdeepbiswas@gmail.com
+"""
+
+import datetime
+
+class Timer:
+    
+    def __init__(self):
+        """
+        Constructor that initializes timestamps to empty strings.
+        """
+        startts = ""
+        endts = ""
+    
+
+    def markStart(self):
+        """
+        Marks start of event and prints out timestamp.
+        """
+        self.startts = datetime.datetime.now()
+        print("started processing:", self.startts)
+    
+
+    def markEnd(self):
+        """
+        Marks end of event and prints out timestamp and the total duration that was taken.
+        """
+        self.endts = datetime.datetime.now()
+        print("done processing:", self.endts)
+        print("this operation took:", self.endts - self.startts)
---
+++ src/clock_keeper/example/usage.py
@@ -0,0 +1,15 @@
+"""
+Author: r4jdeepbiswas@gmail.com
+"""
+
+# Importing Class from module.
+from clock_keeper import Timer
+
+# Instantiating timer object.
+timer = Timer()
+
+# Starting keeping time.
+timer.markStart()
+
+# End keeping time.
+timer.markEnd()
